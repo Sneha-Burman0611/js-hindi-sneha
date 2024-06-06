f38498904332f6fa39b60fe8428aefb88359e544
Sneha-Burman0611 <155543111+Sneha-Burman0611@users.noreply.github.com>
2024-06-06 11:17:33 +0000

    03 cont

--- 01-basics/03_conversionOperations.js
+++ 01-basics/03_conversionOperations.js
@@ -24,3 +24,55 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof (stringNumber));
+
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/3);
+//console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+console.log(str3);
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log("1" + 2 + 2);
+//console.log(1 + 2 + "2");
+
+//console.log(+true); //output 1
+//console.log(+""); //output 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+//console.log(gameCounter);
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+//console.log(1 == 2);
+//console.log(2 != 1);
+//always this type of conversions as it cretes confusion
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined> 0);
+console.log(undefined < 0);
+
+// === 
+console.log("2" == 2);
